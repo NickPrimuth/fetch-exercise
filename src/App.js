feb75b1524629e4085fb94284105cd3275d22cc3
Nick <nbprimuth@gmail.com>
2021-05-31 18:56:35 -0700

    Wrapper and globals added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import { DataDisplay } from './components/DataDisplay';
+import { Globals } from './styles/Globals';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Globals />
+      <Header>
+        <h1>Fetch Rewards Coding Exercise</h1>
+      </Header>
+      <Wrapper>
+        <DataDisplay />
+      </Wrapper>
+    </>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Header = styled.header`
+  /* height: 4em; */
+  color: #ff9900;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
---
+++ src/styles/Globals.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Globals = createGlobalStyle`
+  body {
+    margin: 0;
+    background-color: #F4F4F4;
+    height: 100%;
+    width: 100%;
+  }
+
+
+`;
+
+export { Globals };
